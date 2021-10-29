12af364fc2bf76481488be47117c89980df9b489
Gazz <gaetanthomasdev@gmail.com>
2021-10-29 14:38:18 +0200

    f

--- sketch.js
+++ sketch.js
@@ -4,38 +4,51 @@ let width = innerWidth;
 let height = innerHeight;
 let images = [];
 let lines = [];
-let score = 0;
+let score = 10;
+let freq = 25;
+
 function setup() {
     
     createCanvas(width,height);
     imageMode(CENTER);
-    
     rectMode(CENTER);
-  for (var i = 0; i < 1; i++) { //Boucle pour load images planétes
-    images.push(loadImage(`images/car${i}.png`));
-  }
-    for (var i = 0; i < 10; i++) { //Boucle pour load images planétes
-    lines.push(new Line(width/2, i*height/10));
-    lines.push(new Line(width/3, i*height/10));
-    lines.push(new Line(width/1.5, i*height/10));
-  }
+    for (var i = 0; i < 1; i++) { //Boucle pour load images planétes
+        images.push(loadImage(`images/car${i}.png`));
+    }
+
+    for (var i = 1; i < 11; i++) { //Boucle pour load images planétes
+        lines.push(new Line(width/2, i*height/10));
+        lines.push(new Line(width/3, i*height/10));
+        lines.push(new Line(width/1.5, i*height/10));
+    }
       //Boucle pour load images planétes
    
-    player = new Car(width/2,height*0.66,images[0]);
+    player = new Player(width/2,height*0.66);
 }
 
 
 function draw() {
     
+    if(millis() % 10 == 0){
+        
+        cars.push(new Enemy(random(width*0.25,width*0.75)));
+        
+    }
+    
     background(190);
     fill(100);
     rect(width/2,height/2,width*0.66,height);
+    
+    
+    for(let line of lines){
+        line.display();
+    }
+
+    for(let car of cars){
+        car.display();
+    }
+    
     fill(255);
-    
-for(let line of lines){
-    line.display();
-}
-    
     textSize(25);
     text("Score : " + score, width /2, height /10);
     
