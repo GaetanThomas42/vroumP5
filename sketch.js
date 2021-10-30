b270120e1e3e2165d097c0e305952be2584e58be
gaetanthomasdev <gaetanthomasdev@gmail.com>
2021-10-30 17:23:08 +0200

    speed gestion

--- sketch.js
+++ sketch.js
@@ -51,7 +51,7 @@ function draw() {
     fill(s,0,0); 
     line(width*0.04, height *0.1,map(player.initialSpeed - player.speed,1,player.initialSpeed,-width*0.01,height*0.1),0);
     textSize(width/40);
-    let kmH = parseInt(map(player.initialSpeed - player.speed,1,player.initialSpeed,0,150));
+    let kmH = parseInt(map(player.initialSpeed - player.speed,1,player.initialSpeed,0,200));
     text(kmH, width*0.02, height *0.13);
     player.display();
     
