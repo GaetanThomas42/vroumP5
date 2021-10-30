bb8e8c326b51025cc6029f26a429699f0639089a
gaetanthomasdev <gaetanthomasdev@gmail.com>
2021-10-30 17:14:45 +0200

    speed gestion

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let width = innerWidth;
 let height = innerHeight;
 let images = [];
 let lines = [];
-let score = 10;
+let score = 0;
 let freq = 25;
 
 function setup() {
@@ -17,27 +17,26 @@ function setup() {
     }
 
     for (var i = 1; i < 11; i++) { //Boucle pour load images planétes
-        lines.push(new Line(width/2, i*height/10));
-        lines.push(new Line(width/3, i*height/10));
-        lines.push(new Line(width/1.5, i*height/10));
+        lines.push(new Line(width*0.37, i*height/10));
+        lines.push(new Line(width*0.65, i*height/10));
     }
       //Boucle pour load images planétes
    
-    player = new Player(width/2,height*0.66);
+    player = new Player(width/2,height*0.66,10);
 }
 
 
 function draw() {
-    console.log(player.bullets.length)
-    if(millis() % 10 == 0){
+    
+    if(millis() % 15 == 0){
         
-        cars.push(new Enemy(random(width*0.25,width*0.75)));
+        cars.push(Enemy.prototype.spawn());
         
     }
     
     background(190);
     fill(100);
-    rect(width/2,height/2,width*0.66,height);
+    rect(width/2,height/2,width*0.8,height);
     
     
     for(let line of lines){
@@ -47,14 +46,14 @@ function draw() {
     for(let car of cars){
         car.display();
     }
-
-    fill(255);
-    textSize(25);
-    text("Score : " + score, width /2, height /10);
     
+    let s = map(player.initialSpeed - player.speed,1,player.initialSpeed,0,255)
+    fill(s,0,0); 
+    line(width*0.04, height *0.1,map(player.initialSpeed - player.speed,1,player.initialSpeed,-width*0.01,height*0.1),0);
+    textSize(width/40);
+    let kmH = parseInt(map(player.initialSpeed - player.speed,1,player.initialSpeed,0,150));
+    text(kmH, width*0.02, height *0.13);
     player.display();
-        for(let bullet of player.bullets){
-        bullet.display();
-    }
+    
     
 }
